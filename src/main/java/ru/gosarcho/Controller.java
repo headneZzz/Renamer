696593dd605e26edec441bbaa0e7d0febe3a4a9d
headneZzz <mr.darkilya@mail.ru>
2020-03-27 17:37:30 +0300

    Add TextField with Autocomplete

--- src/main/java/ru/gosarcho/Controller.java
+++ src/main/java/ru/gosarcho/Controller.java
@@ -8,17 +8,20 @@ import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
+    private TextField countFiles;
+    @FXML
+    private TextField startSheet;
+    @FXML
+    private TextField firstLetters;
+    @FXML
     private TextField path;
     @FXML
-    private TextField fond;
-    @FXML
-    private TextField op;
-    @FXML
-    private TextField document;
+    private TextFieldWithAutocomplete document;
     @FXML
     private RadioButton forward;
     @FXML
@@ -30,11 +33,23 @@ public class Controller implements Initializable {
     @FXML
     private Label errorLabel;
 
+    private String url = "jdbc:postgresql://server:5433/archive";
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ToggleGroup toggleGroup = new ToggleGroup();
         forward.setToggleGroup(toggleGroup);
         back.setToggleGroup(toggleGroup);
+        try (Connection connection = DriverManager.getConnection(url, "admin", "adminus")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM digitization.Сводный_журнал");
+            while (resultSet.next()) {
+                document.getEntries().add(resultSet.getString("ФОД").replaceAll("_", " "));
+            }
+        } catch (SQLException e) {
+            errorLabel.setText("Ошибка подлючения к базе данных");
+            e.printStackTrace();
+        }
     }
 
     @FXML
---
+++ src/main/java/ru/gosarcho/TextFieldWithAutocomplete.java
@@ -0,0 +1,101 @@
+package ru.gosarcho;
+
+import javafx.geometry.Side;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class TextFieldWithAutocomplete extends TextField {
+    private SortedSet<String> entries;
+    private ContextMenu entriesPopup;
+
+
+    public TextFieldWithAutocomplete() {
+        super();
+        this.entries = new TreeSet<>();
+        this.entriesPopup = new ContextMenu();
+        setListner();
+    }
+
+    public SortedSet<String> getEntries() { return entries; }
+
+    private void setListner() {
+        //Add "suggestions" by changing text
+        textProperty().addListener((observable, oldValue, newValue) -> {
+            String enteredText = getText();
+            if (enteredText == null || enteredText.isEmpty()) {
+                entriesPopup.hide();
+            } else {
+                //filter all possible suggestions depends on "Text", case insensitive
+                List<String> filteredEntries = entries.stream()
+                        .filter(e -> e.toLowerCase().contains(enteredText.toLowerCase()))
+                        .collect(Collectors.toList());
+                //some suggestions are found
+                if (!filteredEntries.isEmpty()) {
+                    //build popup - list of "CustomMenuItem"
+                    populatePopup(filteredEntries, enteredText);
+                    if (!entriesPopup.isShowing()) { //optional
+                        entriesPopup.show(TextFieldWithAutocomplete.this, Side.BOTTOM, 0, 0); //position of popup
+                    }
+                    //no suggestions -> hide
+                } else {
+                    entriesPopup.hide();
+                }
+            }
+        });
+
+        //Hide always by focus-in (optional) and out
+        focusedProperty().addListener((observableValue, oldValue, newValue) -> entriesPopup.hide());
+    }
+
+    private void populatePopup(List<String> searchResult, String searchReauest) {
+        //List of "suggestions"
+        List<CustomMenuItem> menuItems = new LinkedList<>();
+        int maxEntries = 10;
+        int count = Math.min(searchResult.size(), maxEntries);
+        //Build list as set of labels
+        for (int i = 0; i < count; i++) {
+            final String result = searchResult.get(i);
+            //label with graphic (text flow) to highlight founded subtext in suggestions
+            Label entryLabel = new Label();
+
+            entryLabel.setGraphic(buildTextFlow(result, searchReauest));
+            entryLabel.setPrefHeight(10);  //don't sure why it's changed with "graphic"
+            CustomMenuItem item = new CustomMenuItem(entryLabel, true);
+            menuItems.add(item);
+
+            //if any suggestion is select set it into text and close popup
+            item.setOnAction(actionEvent -> {
+                setText(result);
+                positionCaret(result.length());
+                entriesPopup.hide();
+            });
+        }
+
+        //"Refresh" context menu
+        entriesPopup.getItems().clear();
+        entriesPopup.getItems().addAll(menuItems);
+    }
+
+    private TextFlow buildTextFlow(String text, String filter) {
+        int filterIndex = text.toLowerCase().indexOf(filter.toLowerCase());
+        Text textBefore = new Text(text.substring(0, filterIndex));
+        Text textAfter = new Text(text.substring(filterIndex + filter.length()));
+        Text textFilter = new Text(text.substring(filterIndex, filterIndex + filter.length())); //instead of "filter" to keep all "case sensitive"
+        textFilter.setFill(Color.BLUE);
+        textFilter.setFont(Font.font("Helvetica", FontWeight.BOLD, 12));
+        return new TextFlow(textBefore, textFilter, textAfter);
+    }
+}
